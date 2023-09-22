6656496f3fee691a6c5918fe3a3ca66724765d44
kimseojeong <0521kimsj@gmail.com>
2023-09-22 22:20:47 +0900

    .

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 $(function (){
-    $('.visual_slide').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
+  // 슬릭 슬라이드
+  $('.visual_slide').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+
+    //header
+    if ($(window).width() < 1024) {
+      $('.out').hide();
+      $('.mb').click(function(){
+        $('.gnb').addClass('on')
       });
+    }
 })
