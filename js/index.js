a22c570304f5269b9bda1e06b5f5e1f9d49d5bd5
kimseojeong <0521kimsj@gmail.com>
2024-06-20 11:05:44 +0900

    6.20 업데이트
    <업데이트 내용>
    -메인 페이지 세션 2 퍼블리싱

--- js/index.js
+++ js/index.js
@@ -13,5 +13,28 @@ $(function (){
       $('.mb').click(function(){
         $('.gnb').addClass('on')
       });
-    }
+    };
+
+    //베스트 상품
+    if ($('.best_one').mouseenter(function(){
+      $('.leanMore').addClass('on');
+      $('.favorite').addClass('on');
+      if ($('.leanMore').mouseenter(function(){
+        $('.leanMore').addClass('on');
+      }));
+      if ($('.favorite').mouseenter(function(){
+        $('.favorite').addClass('on');
+      }));
+    }));
+    if ($('.best_one').mouseout(function(){
+      $('.leanMore').removeClass('on');
+      $('.favorite').removeClass('on');
+      if ($('.leanMore').mouseenter(function(){
+        $('.leanMore').addClass('on');
+      }));
+      if ($('.favorite').mouseenter(function(){
+        $('.favorite').addClass('on');
+      }));
+    }));
+
 })
