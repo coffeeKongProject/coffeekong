e1a34c8a53f498378f2c15a4ec5c5e46000b24ad
kimseojeong <0521kimsj@gmail.com>
2024-06-21 11:09:20 +0900

    6.21 업데이트
    <업데이트 내용>
    -메인 페이지 세션2 완료
    -추후 코드 최적화 필요

--- js/index.js
+++ js/index.js
@@ -15,12 +15,12 @@ $(function (){
       });
     };
 
-    //베스트 상품
+    //베스트 1
     if ($('.best_one').mouseenter(function(){
       $('.best_one').addClass('on');
       $('.best_one.on').show();
-      $('<a href="#" class="learnMore"></a>').prependTo('.best_one.on').css({display: "block"});
-      $('<a href="#" class="favorite"></a>').appendTo('.best_one.on').css({display: "block"});
+      $('<a href="#" class="learnMore one"></a>').prependTo('.best_one.on').css({display: "block"});
+      $('<a href="#" class="favorite one"></a>').appendTo('.best_one.on').css({display: "block"});
       $('.best_one_link').css({width: "0", height: "0"})
       if($('.best_one').mouseleave(function(){
         $('.best_one').removeClass('on');
@@ -28,5 +28,32 @@ $(function (){
         $('.favorite').css({display: "none"});
       }));
     }));
+    // 베스트 2
+    if ($('.best_two').mouseenter(function(){
+      $('.best_two').addClass('on');
+      $('.best_two.on').show();
+      $('<a href="#" class="learnMore two"></a>').prependTo('.best_two.on').css({display: "block"});
+      $('<a href="#" class="favorite two"></a>').appendTo('.best_two.on').css({display: "block"});
+      $('.best_two_link').css({width: "0", height: "0"})
+      if($('.best_two').mouseleave(function(){
+        $('.best_two').removeClass('on');
+        $('.learnMore').css({display: "none"});
+        $('.favorite').css({display: "none"});
+      }));
+    }));
+    // 베스트 3
+    if ($('.best_three').mouseenter(function(){
+      $('.best_three').addClass('on');
+      $('.best_three.on').show();
+      $('<a href="#" class="learnMore three"></a>').prependTo('.best_three.on').css({display: "block"});
+      $('<a href="#" class="favorite three"></a>').appendTo('.best_three').css({display: "block"});
+      $('.best_three_link').css({width: "0", height: "0"})
+      if($('.best_three').mouseleave(function(){
+        $('.best_three').removeClass('on');
+        $('.learnMore').css({display: "none"});
+        $('.favorite').css({display: "none"});
+      }));
+    }));
+    // 최적화 필요(간결히)
 
 })
