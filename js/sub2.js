2257093b240068cd08ae06aaa990b8d7f0d0d1ab
KimSeoJeong <0521kimsj@gmail.com>
2024-07-26 11:30:04 +0900

    7.26 업데이트
    상세페이지 구현
    -더보기, 접기
    -탑 버튼

--- js/sub2.js
+++ js/sub2.js
@@ -7,7 +7,7 @@ $(function () {
         if ($('#id, #password').focusin()) {
             console.log("이벤트 감지");
             $('.id').off('click');
-        } 
+        }
         // 기본적인 토글은 됐으면 좋겠는데 off() 때문에 뻐김
         // else if ($(this).focusout()) {
         //     console.log("커서 나감");
@@ -22,6 +22,32 @@ $(function () {
             $('.gnb').toggle('slow')
         });
     }
+
+    // -------- swiper --------- //
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,
+        spaceBetween: 4,
+        slidesPerView: 5,
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".mySwiper2", {
+        loop: true,
+        spaceBetween: 4,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: swiper,
+        },
+    });
+
+    // 탑 버튼
+    $(".go_up").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 300);
+    });
+
 });
 
 /* ------------------------- */
@@ -41,4 +67,47 @@ $(window).resize(function () {
         lastWidth = $(window).width();
         return false;
     }
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () { //DOM 생성 후 이벤트 리스너 등록    
+
+    //더 보기 버튼 이벤트 리스너    
+    document.querySelector('.more').addEventListener('click', function (e) {
+        // 더 보기 프레임의 클래스 정보 얻기        
+        let classList = document.querySelector('.goods_detail').classList; 
+        // 컨텐츠 높이 얻기
+        let contentHeight = document.querySelector('.goods_detail').offsetHeight;  
+
+        // 2단계이면 전체 보기로        
+        if (classList.contains('cut02')) {
+            classList.remove('cut02');
+        }
+
+        // 1단계이면 2단계로        
+        if (classList.contains('cut01')) {
+            classList.remove('cut01');
+            if (contentHeight > 600) {
+                classList.add('cut02');
+            } else {
+                document.querySelector('.more').classList.add('hide');
+            }
+        }
+
+        //전체보기 시 더 보기 버튼 감추기 & 감추기 버튼 표시        
+        if (!classList.contains('cut01') && !classList.contains('cut02')) {
+            e.target.classList.add('hide');
+            document.querySelector('.less').classList.remove('hide');
+        }
+    });
+});
+
+// 감추기 버튼 이벤트 리스너
+document.querySelector('.less').addEventListener('click', function (e) {
+    e.target.classList.add('hide');
+    // 더 보기 버튼 감춤   
+    document.querySelector('.more').classList.remove('hide');
+    // 초기 감춤 상태로 복귀
+    document.querySelector('.goods_detail').classList.add('cut01');
 });
